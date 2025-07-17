3265056fce4fbb8a5d1f5a62e6ced38df8bdfa40
aayush23091 <aaayushyadav0@gmail.com>
2025-07-17 17:38:32 +0545

    LoginActivity updated

--- app/src/main/java/com/example/sportsequipmentstore/view/LoginActivity.kt
+++ app/src/main/java/com/example/sportsequipmentstore/view/LoginActivity.kt
@@ -1,3 +1,277 @@
+//package com.example.sportsequipmentstore
+//
+//import android.app.Activity
+//import android.content.Context
+//import android.content.Intent
+//import android.os.Bundle
+//import android.widget.Toast
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.activity.enableEdgeToEdge
+//import androidx.compose.foundation.Image
+//import androidx.compose.foundation.background
+//import androidx.compose.foundation.clickable
+//import androidx.compose.foundation.layout.*
+//import androidx.compose.foundation.shape.RoundedCornerShape
+//import androidx.compose.foundation.text.KeyboardOptions
+//import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.Email
+//import androidx.compose.material.icons.filled.Lock
+//import androidx.compose.material3.*
+//import androidx.compose.runtime.*
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.platform.LocalContext
+//import androidx.compose.ui.res.painterResource
+//import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.text.input.KeyboardType
+//import androidx.compose.ui.text.input.PasswordVisualTransformation
+//import androidx.compose.ui.text.input.VisualTransformation
+//import androidx.compose.ui.text.style.TextAlign
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.unit.sp
+//import com.example.sportsequipmentstore.repository.UserRepositoryImplementation
+//import com.example.sportsequipmentstore.view.DashboardActivity
+//import com.example.sportsequipmentstore.view.ForgetPasswordActivity
+//import com.example.sportsequipmentstore.view.RegistrationActivity
+//import com.example.sportsequipmentstore.viewmodel.UserViewModel
+//import kotlinx.coroutines.launch
+//
+//class LoginActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContent {
+//            LoginBody()
+//        }
+//    }
+//}
+//
+//@Composable
+//fun LoginBody() {
+//    val repo = remember { UserRepositoryImplementation() }
+//    val userViewModel = remember { UserViewModel(repo) }
+//
+//    var email by remember { mutableStateOf("") }
+//    var password by remember { mutableStateOf("") }
+//    var passwordVisibility by remember { mutableStateOf(false) }
+//    var rememberMe by remember { mutableStateOf(false) }
+//
+//    val context = LocalContext.current
+//    val activity = context as Activity
+//
+//    val sharedPreferences = context.getSharedPreferences("User", Context.MODE_PRIVATE)
+//    val editor = sharedPreferences.edit()
+//
+//    val localEmail: String = sharedPreferences.getString("email", "").toString()
+//    val localPassword: String = sharedPreferences.getString("password", "").toString()
+//
+//    email = localEmail
+//    password = localPassword
+//
+//    val coroutineScope = rememberCoroutineScope()
+//    val snackbarHostState = remember { SnackbarHostState() }
+//
+//    var showDialog by remember { mutableStateOf(false) }
+//
+//    Scaffold(
+//        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+//    ) { padding ->
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(padding)
+//                .background(color = Color.Green),
+//            horizontalAlignment = Alignment.CenterHorizontally
+//        ) {
+//            Spacer(modifier = Modifier.height(50.dp))
+//            Text(
+//                text = "RetroCrugSports",
+//                fontSize = 24.sp,
+//                fontWeight = FontWeight.Bold,
+//                color = Color.Black,
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(bottom = 10.dp),
+//                textAlign = TextAlign.Center
+//            )
+//
+//            Image(
+//                painter = painterResource(R.drawable.hero),
+//                contentDescription = null
+//            )
+//
+//            Spacer(modifier = Modifier.height(40.dp))
+//
+//            OutlinedTextField(
+//                value = email,
+//                onValueChange = { email = it },
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(horizontal = 15.dp),
+//                placeholder = { Text(text = "Enter email") },
+//                colors = TextFieldDefaults.colors(
+//                    focusedContainerColor = Color.LightGray.copy(alpha = 0.2f),
+//                    focusedIndicatorColor = Color.Blue,
+//                    unfocusedContainerColor = Color.LightGray.copy(alpha = 0.2f),
+//                    unfocusedIndicatorColor = Color.Black
+//                ),
+//                shape = RoundedCornerShape(12.dp),
+//                prefix = { Icon(Icons.Default.Email, contentDescription = null) },
+//                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+//            )
+//
+//            Spacer(modifier = Modifier.height(20.dp))
+//
+//            OutlinedTextField(
+//                value = password,
+//                onValueChange = { password = it },
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(horizontal = 15.dp),
+//                placeholder = { Text(text = "Enter password") },
+//                colors = TextFieldDefaults.colors(
+//                    focusedContainerColor = Color.LightGray.copy(alpha = 0.2f),
+//                    focusedIndicatorColor = Color.Blue,
+//                    unfocusedContainerColor = Color.LightGray.copy(alpha = 0.2f),
+//                    unfocusedIndicatorColor = Color.Black
+//                ),
+//                shape = RoundedCornerShape(12.dp),
+//                prefix = { Icon(Icons.Default.Lock, contentDescription = null) },
+//                suffix = {
+//                    Icon(
+//                        painterResource(
+//                            if (passwordVisibility) R.drawable.baseline_visibility_24
+//                            else R.drawable.baseline_visibility_off_24
+//                        ),
+//                        contentDescription = null,
+//                        modifier = Modifier.clickable { passwordVisibility = !passwordVisibility }
+//                    )
+//                },
+//                visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+//                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+//            )
+//
+//            Spacer(modifier = Modifier.height(10.dp))
+//
+//            Row(
+//                modifier = Modifier
+//                    .padding(horizontal = 12.dp)
+//                    .fillMaxWidth(),
+//                verticalAlignment = Alignment.CenterVertically,
+//                horizontalArrangement = Arrangement.SpaceBetween
+//            ) {
+//                Row(verticalAlignment = Alignment.CenterVertically) {
+//                    Checkbox(
+//                        colors = CheckboxDefaults.colors(
+//                            checkedColor = Color.Blue,
+//                            checkmarkColor = Color.LightGray
+//                        ),
+//                        checked = rememberMe,
+//                        onCheckedChange = { rememberMe = it }
+//                    )
+//                    Text("Remember me")
+//                }
+//
+//                Text(
+//                    text = "Forget Password?",
+//                    color = Color.Blue,
+//                    modifier = Modifier.clickable {
+//                        val intent = Intent(context, ForgetPasswordActivity::class.java)
+//                        context.startActivity(intent)
+//                        activity.finish()
+//                    }
+//                )
+//            }
+//
+//            Spacer(modifier = Modifier.height(10.dp))
+//
+//            Button(
+//                onClick = {
+//                    userViewModel.login(email, password) { success, message ->
+//                        if (success) {
+//                            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+//                            val intent = Intent(context, DashboardActivity::class.java)
+//                            context.startActivity(intent)
+//                            activity.finish()
+//                        } else {
+//                            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+//                        }
+//                    }
+//
+//                    if (email == "ram@gmail.com" && password == "password") {
+//                        if (rememberMe) {
+//                            editor.putString("email", email)
+//                            editor.putString("password", password)
+//                            editor.apply()
+//                        }
+//                        activity.finish()
+//                        Toast.makeText(context, "Login success", Toast.LENGTH_SHORT).show()
+//                    } else {
+//                        coroutineScope.launch {
+//                            snackbarHostState.showSnackbar("Invalid login")
+//                        }
+//                    }
+//                },
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(horizontal = 20.dp),
+//                shape = RoundedCornerShape(10.dp)
+//            ) {
+//                Text("Login")
+//            }
+//
+//            Text(
+//                "Don't have an account, Signup",
+//                modifier = Modifier
+//                    .padding(top = 16.dp)
+//                    .clickable {
+//                        val intent = Intent(context, RegistrationActivity::class.java)
+//                        context.startActivity(intent)
+//                        activity.finish()
+//                    }
+//            )
+//
+//            // Show AlertDialog Button just below signup text
+//            Button(
+//                onClick = { showDialog = true },
+//                modifier = Modifier
+//                    .padding(top = 16.dp)
+//            ) {
+//                Text("Show AlertDialog")
+//            }
+//
+//            if (showDialog) {
+//                AlertDialog(
+//                    onDismissRequest = { showDialog = false },
+//                    confirmButton = {
+//                        Button(onClick = { showDialog = false }) {
+//                            Text("OK")
+//                        }
+//                    },
+//                    dismissButton = {
+//                        Button(onClick = { showDialog = false }) {
+//                            Text("Cancel")
+//                        }
+//                    },
+//                    title = { Text(text = "Alert Title") },
+//                    text = { Text("This is an alert dialog message.") }
+//                )
+//            }
+//        }
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun PreviewLogin() {
+//    LoginBody()
+//}
+
+
+
 package com.example.sportsequipmentstore
 
 import android.app.Activity
@@ -29,13 +303,10 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.sportsequipmentstore.repository.UserRepositoryImplementation
-import com.example.sportsequipmentstore.view.DashboardActivity
-import com.example.sportsequipmentstore.view.ForgetPasswordActivity
-import com.example.sportsequipmentstore.view.RegistrationActivity
+import com.example.sportsequipmentstore.view.*
 import com.example.sportsequipmentstore.viewmodel.UserViewModel
 import kotlinx.coroutines.launch
 
@@ -64,12 +335,6 @@ fun LoginBody() {
 
     val sharedPreferences = context.getSharedPreferences("User", Context.MODE_PRIVATE)
     val editor = sharedPreferences.edit()
-
-    val localEmail: String = sharedPreferences.getString("email", "").toString()
-    val localPassword: String = sharedPreferences.getString("password", "").toString()
-
-    email = localEmail
-    password = localPassword
 
     val coroutineScope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
@@ -112,12 +377,6 @@ fun LoginBody() {
                     .fillMaxWidth()
                     .padding(horizontal = 15.dp),
                 placeholder = { Text(text = "Enter email") },
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.LightGray.copy(alpha = 0.2f),
-                    focusedIndicatorColor = Color.Blue,
-                    unfocusedContainerColor = Color.LightGray.copy(alpha = 0.2f),
-                    unfocusedIndicatorColor = Color.Black
-                ),
                 shape = RoundedCornerShape(12.dp),
                 prefix = { Icon(Icons.Default.Email, contentDescription = null) },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
@@ -132,17 +391,11 @@ fun LoginBody() {
                     .fillMaxWidth()
                     .padding(horizontal = 15.dp),
                 placeholder = { Text(text = "Enter password") },
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.LightGray.copy(alpha = 0.2f),
-                    focusedIndicatorColor = Color.Blue,
-                    unfocusedContainerColor = Color.LightGray.copy(alpha = 0.2f),
-                    unfocusedIndicatorColor = Color.Black
-                ),
                 shape = RoundedCornerShape(12.dp),
                 prefix = { Icon(Icons.Default.Lock, contentDescription = null) },
                 suffix = {
                     Icon(
-                        painterResource(
+                        painter = painterResource(
                             if (passwordVisibility) R.drawable.baseline_visibility_24
                             else R.drawable.baseline_visibility_off_24
                         ),
@@ -165,10 +418,6 @@ fun LoginBody() {
             ) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     Checkbox(
-                        colors = CheckboxDefaults.colors(
-                            checkedColor = Color.Blue,
-                            checkmarkColor = Color.LightGray
-                        ),
                         checked = rememberMe,
                         onCheckedChange = { rememberMe = it }
                     )
@@ -193,25 +442,26 @@ fun LoginBody() {
                     userViewModel.login(email, password) { success, message ->
                         if (success) {
                             Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-                            val intent = Intent(context, DashboardActivity::class.java)
-                            context.startActivity(intent)
+
+                            if (email == "ram@gmail.com") {
+                                val intent = Intent(context, DashboardActivity::class.java)
+                                context.startActivity(intent)
+                            } else {
+                                val intent = Intent(context, UserDashboardActivity::class.java)
+                                context.startActivity(intent)
+                            }
+
+                            if (rememberMe) {
+                                editor.putString("email", email)
+                                editor.putString("password", password)
+                                editor.apply()
+                            }
+
                             activity.finish()
                         } else {
-                            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-                        }
-                    }
-
-                    if (email == "ram@gmail.com" && password == "password") {
-                        if (rememberMe) {
-                            editor.putString("email", email)
-                            editor.putString("password", password)
-                            editor.apply()
-                        }
-                        activity.finish()
-                        Toast.makeText(context, "Login success", Toast.LENGTH_SHORT).show()
-                    } else {
-                        coroutineScope.launch {
-                            snackbarHostState.showSnackbar("Invalid login")
+                            coroutineScope.launch {
+                                snackbarHostState.showSnackbar("Invalid login: $message")
+                            }
                         }
                     }
                 },
@@ -234,7 +484,6 @@ fun LoginBody() {
                     }
             )
 
-            // Show AlertDialog Button just below signup text
             Button(
                 onClick = { showDialog = true },
                 modifier = Modifier
@@ -256,16 +505,10 @@ fun LoginBody() {
                             Text("Cancel")
                         }
                     },
-                    title = { Text(text = "Alert Title") },
+                    title = { Text("Alert Title") },
                     text = { Text("This is an alert dialog message.") }
                 )
             }
         }
     }
-}
-
-@Preview
-@Composable
-fun PreviewLogin() {
-    LoginBody()
 }
