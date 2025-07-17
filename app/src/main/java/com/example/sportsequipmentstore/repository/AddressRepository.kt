2f8a64177a02e9c2ee8689ebf8064cb5bf641be3
aayush23091 <aaayushyadav0@gmail.com>
2025-07-17 23:00:08 +0545

    updated

--- app/src/main/java/com/example/sportsequipmentstore/repository/AddressRepository.kt
+++
@@ -1,7 +0,0 @@
-import com.example.sportsequipmentstore.model.AddressModel
-
-interface AddressRepository {
-    fun addAddress(address: AddressModel, callback: (Boolean, String) -> Unit)
-    fun getAllAddresses(callback: (List<AddressModel>) -> Unit)
-    fun deleteAddress(addressId: String, callback: (Boolean, String) -> Unit)
-}
