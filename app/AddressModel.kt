3d685680ff3ebf6eb8ab1b4fb985c97e4e81d8ca
aayush23091 <aaayushyadav0@gmail.com>
2025-07-19 14:07:16 +0545

    AddressActivity  added

---
+++ app/AddressModel.kt
@@ -0,0 +1,10 @@
+package com.example.sportsequipmentstore.model
+
+data class AddressModel(
+    val id: String = "",
+    val userId: String = "",
+    val street: String = "",
+    val city: String = "",
+    val postalCode: String = "",
+    val country: String = ""
+)
